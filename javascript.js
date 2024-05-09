01c198f2da51e99ce199663d3a9f496b9226564e
martinaliporata <martinaliporata@gmail.com>
2024-05-09 07:51:07 +0200

    added discount

--- javascript.js
+++ javascript.js
@@ -6,8 +6,27 @@
 // L'output del prezzo finale va messo fuori in forma umana 
 // (con massimo due decimali, per indicare centesimi sul prezzo) [questo richiederà un minimo di ricerca]
 
-const kmUser = prompt('Quanti km vuoi percorrere')
+const kmUser = prompt('Quanti km vuoi percorrere');
+console.log(kmUser);
 
-const ageUser = prompt('Quanti anni ha il passeggero')
+const ageUser = prompt('Quanti anni ha il passeggero');
+ageUser = parseInt(ageUser);
+console.log(ageUser);
 
-kmUser * 0.276
+// kmUser * 0.276 questo è il prezzo
+
+if ageUser<18 {
+    let sconto = 21
+}
+
+let scontoSulPrezzo = (prezzo/100) * sconto
+let prezzoScontato = prezzo - scontoSulPrezzo
+console.log(sconto, prezzoScontato)
+
+if ageUser>65 {
+    let sconto = 42
+}
+
+let scontoSulPrezzo = (prezzo/100) * sconto
+let prezzoScontato = prezzo - scontoSulPrezzo
+console.log(sconto, prezzoScontato)
